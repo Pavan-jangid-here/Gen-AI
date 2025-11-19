5f7a1101c743148d5a2a815a2698578acb767798
pavan-jangid-here <pavan5.jan@gmail.com>
2025-11-19 22:12:33 +0530

    setting the envs

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative-AI Project",
+    version = "0.0.0",
+    author = "Pavan Jangid",
+    author_email = "pavan5.jan@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
